b684a10d1aa74e7b91291e3cf42e5c320c6bb327
Luy Mattos <112789465+LuyMattosS@users.noreply.github.com>
2023-05-23 11:34:19 -0300

    atualização front

--- projeto_escala/index.php
+++ projeto_escala/index.php
@@ -1,1 +1,40 @@
-<a href="/projeto_escala/Escala-Medica/projeto_escala/login.php" ><button>Login</button></a>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Escala.Net</title>
+     <!-- Bootstrap -->
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+     <!-- CSS -->
+          <link rel="stylesheet" href="css/index.css">
+</head>
+     <body class="wallpaper"> 
+          <header class="hdr"> <!--Cabeçalho-->
+               <figure class="logo">
+                    <img src="img/logo.png">
+               </figure>
+               
+               <nav class="menu"> <!--Menu-->
+                    <div class="packge-menu">
+                         <section class="pack-menu">
+                              <a href="index.php"><p type="button">Inicio</p>
+                              <a href="sobrenos.html"><p type="button" >Sobre Nós</p></a>
+                              <a href="login.php" class="link-login"><button type="login">Login</button></a>
+                         </section>
+                    </div>
+               </nav>
+          </header>
+
+          <main class="packege"> <!--Conteudo-->
+               <h1>Sua agenda de gerenciamento <br/>de profissionais da saúde.</h1>
+          </main> <!--Fim do Main-->
+
+          <footer class="rodape"><!--Roda Pé-->
+               <hr/>
+               <p>Projeto de Laboratorio de Inovação V • 1/2023</p>
+          </footer>
+     </body>
+</html>
--- projeto_escala/login.php
+++ projeto_escala/login.php
@@ -1,27 +1,75 @@
 <!DOCTYPE html>
-<html lang="pt-br" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Login</title>
-  </head>
-  <body>
+<html lang="pt-br">
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Acesso ao Sistema</title>
+     <!-- Bootstrap -->
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+     <!-- CSS -->
+     <link rel="stylesheet" href="css/login.css">
+</head>
+<body class="wallpaper">
 
-        <h1>Olá, seja bem-vindo(a)!!</h1>
-        <br><br>
-      <form action="loginConfig.php" method="post">
+     <header class="hdr"> <!--Cabeçalho-->
+          <figure class="logo">
+               <img src="img/logo.png">
+          </figure>
+               
+          <nav class="menu"> <!--Menu-->
+               <div class="packge-menu">
+                    <section class="pack-menu">
+                         <a href="index.php"><p type="button">Inicio</p>
+                         <a href="sobrenos.html"><p type="button" >Sobre Nós</p></a>
+                         <a href="index.php" class="link-login"><button type="login">Voltar</button></a>
+                    </section>
+               </div>
+          </nav>
+     </header>
 
-          <label for="matricula">Matricula</label>
-          <input type="matricula" name="matricula" id="matricula">
+     <h2 class="titulo">Faça seu Login</h2>   
+     <div class="container"> <!-- Conteúdo | classe externa Bootstrap -->
+          <div class="row justify-content-center mt-5">  <!-- Coluna do Bootstrap | classe externa Bootstrap-->
 
-          <br><br>
+               <div class="packege"> <!-- Tela de Login | classe interna-->
 
-          <label for="senha">Senha</label>
-          <input type="password" name="senha" id="senha" class="form-control">
+                    <section>
+                         <form class="form-login" action="loginConfig.php" method="post"> <!-- Formulario de Login| Classe Interna-->
+                              <!-- Matricúla / Senha | classe interna-->
+                              <div class="packege-inputs"> 
+                                   
+                                   <div class="packege-matri">
+                                        <label for="matricula">Matricúla</label>
+                                        <br/>
+                                        <input type="matricula" name="matricula" id="matricula" class="btn btn-outline-light">
+                                   </div>
 
-          <br><br>
-          
-        <button type="submit" name="enviarDados">Entrar</button>
-      </form>
-    </div>
-  </body>
+                                   <div class="packege-pass">
+                                        <label for="senha">Senha</label>
+                                        <br/>
+                                        <input type="password" name="senha" id="senha" class="btn btn-outline-light">
+                                   </div>
+                              </div> <!-- Fim Matricúla / Senha -->
+                              
+                              <!-- Botão Etrar | classe externa Bootstrap-->
+                              <div class="btn">
+                                   <div class="text-center mt-5" >  
+                                        <button type="submit" name="" class="btn btn-outline-light btn-sm btn-block" style="margin-bottom: 23px;">Entrar</button>
+                                   </div>  <!-- Fim Botão Entrar --> 
+                              </div>
+                         </form> <!-- Fim class form-login -->
+                    </section>
+               </div> <!-- Fim class login -->
+          </div> <!-- Fim class row -->    
+     </div> <!-- Fim class container -->
+
+     <section><!-- Seção Recuperar senha | classe interna-->
+          <div class="recuperar">
+               <label for="recup">Esqueceu sua senha?</label>
+               <br/>
+               <a href="recsenha.php"><button>Clique aqui!</button></a>
+          </div>
+     </section>
+</body>
 </html>
