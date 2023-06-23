381fd0365036a3b9ef8419b62b8c71fa29fdec42
Luy Mattos <112789465+LuyMattosS@users.noreply.github.com>
2023-06-23 19:04:26 -0300

    Gerenciamento de Servidores

--- index_logado_adm.php
+++ index_logado_adm.php
@@ -3,16 +3,16 @@ require_once 'Conexao.php'; //Puxa a conexão do banco
 
 session_start();//Inicia a seção
 
-if (empty($_SESSION)) {
+//if (empty($_SESSION)) {
   // Significa que as variáveis de SESSAO não foram definidas.
   // Não poderia acessar aqui.
-  header("Location: index.php?msgErro=Você precisa se autenticar no sistema.");
-  die();
-}
-elseif ($_SESSION['adm'] == 'NAO'){
-  header("Location: index_logado_serv.php");
-  die();
-}
+  //header("Location: index.php?msgErro=Você precisa se autenticar no sistema.");
+  //die();
+//}
+//elseif ($_SESSION['adm'] == 'NAO'){
+  //header("Location: index_logado_serv.php");
+  //die();
+//}
 
 $sql = "SELECT * FROM serv_log ORDER BY id_serv ASC";
   try {
@@ -33,10 +33,19 @@ $sql = "SELECT * FROM serv_log ORDER BY id_serv ASC";
 <!DOCTYPE html>
 <html lang="pt-br" dir="ltr">
   <head>
-    <meta charset="utf-8">
-    <title>Página Inicial - Ambiente Logado</title>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Administração</title>
+      <!-- Icon Google -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,1,0" />
+      <!-- Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <!-- CSS -->
+          <link rel="stylesheet" href="css/index_logado_adm.css">
   </head>
-  <body>
+  <body class="wallpaper">
     <?php if (!empty($_GET['msgErro'])) 
     { 
       echo $_GET['msgErro'];
@@ -48,56 +57,75 @@ $sql = "SELECT * FROM serv_log ORDER BY id_serv ASC";
     }
     ?>
 
-    <div>
-      <a href="Logout.php" class="">Sair</a>
-    </div>
-    
-    <h2>Olá <i><?php echo $_SESSION['nome']; ?></i>, seja bem-vindo(a)!</h2>
-    
-    <div>
-      <a href="list_esp.php" class="">Listar Especialidades</a>
-    </div>
-    <div>
-      <a href="tabela_adm.php" class="">Escala Medica</a>
-    </div>
+      <header class="hdr"> <!-- Cabeçalho / Menu -->
+        <figure class="logo">
+            <img src="img/logo.png">
+        </figure>
+                
+          <nav class="menu"> <!--Menu-->
+              <div class="packge-menu">
+                  <section class="pack-menu">
+                      <a href="index.php"><p type="button">Inicio</p></a>
+                      <a href="tabela_adm.php"><p type="button">Escala</p></a>
+                      <a href="cad_serv.php"><p type="button">Cadastro</p></a>
+                      <a href="list_esp.php" class="">Especialidades</a>
+                      <a href="sobre.php"><p type="button" >Sobre</p></a>
+                      <!--p><i><?//php echo $_SESSION['nome']; ?></i> - Conectado</p-->
+                      <a href="Logout.php" class="link-login"><button type="login">Sair</button></a>
+                  </section>
+              </div>
+          </nav>
+      </header>
 
-    <h3>Listagem de servidores</h3>
-    
-    <a href="cad_serv.php" class="">Cadastrar Servidor</a>
-
-    <?php if (!empty($servidores)) { ?>
-      <!-- Aqui que será montada a tabela com a relação de servidores!! -->
-
-      
-      <br><br>
-      
-      <table>
-          <thead> 
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Nome</th>
-              <th scope="col">CPF</th>
-              <th scope="col">Matricula</th>
-              <th scope="col">Email</th>
-              <th scope="col">Ações</th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php foreach ($servidores as $s) { ?>
-              <tr>
-                <th scope="row"><?php echo $s['id_serv']; ?></th>
-                <td><?php echo $s['nome']; ?></td>
-                <td><?php echo $s['cpf']; ?></td>
-                <td><?php echo $s['matricula']; ?></td>
-                <td><?php echo $s['email']; ?></td>
-                <td>
-                    <a href="alt_serv.php?id_serv=<?php echo $s['id_serv']; ?>">Alterar</a>
-                    <a href="del_serv.php?id_serv=<?php echo $s['id_serv']; ?>">Excluir</a>
-                </td>
-              </tr>
-            <?php } ?>
-          </tbody>
-        </table>
+    <main>
+    <div class="packege-tab">
+      <h1>Gerenciar Servidor</h1>
+                    
+      <!-- Botão para abrir Modal de Pesquisa -->
+      <button class="material-symbols-outlined btn-search">
+        <a href="#">
+          <p>Pesquisar Servidor</p>
+          <span class="material-symbols-outlined icon-search">search</span>
+        </a>
+      </button>
+      <?php if (!empty($servidores)) { ?>
+        <!-- Aqui que será montada a tabela com a relação de servidores!! -->
+        
+        <div class="pack-tab">
+            <table class="table table-hover"> <!-- Tabela referente ao manter Cadastro de servidor -->
+              <thead> <!-- Cabeçalho -->
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Nome</th>
+                  <th scope="col">CPF</th>
+                  <th scope="col">Matricula</th>
+                  <th scope="col">Email</th>
+                  <th scope="col"></th>
+                  <th scope="col"></th>
+                </tr>
+              </thead>
+            <tbody>
+              <?php foreach ($servidores as $s) { ?>
+                <tr>
+                  <th scope="row"><?php echo $s['id_serv']; ?></th>
+                  <td><?php echo $s['nome']; ?></td>
+                  <td><?php echo $s['cpf']; ?></td>
+                  <td><?php echo $s['matricula']; ?></td>
+                  <td><?php echo $s['email']; ?></td>
+                  <td>
+                    <div class="btn-edit">
+                        <a href="alt_serv.php?id_serv=<?php echo $s['id_serv']; ?>"><span class="material-symbols-outlined">edit</span></a>
+                    </div>
+                    <div class="btn-del">
+                        <a href="del_serv.php?id_serv=<?php echo $s['id_serv']; ?>"><span class="material-symbols-outlined">delete</span></a>
+                    </div>
+                  </td>
+                </tr>
+              <?php } ?>
+            </tbody>
+          </table>
         <?php } ?>
+      </div>      
+    </main>
   </body>
 </html>
