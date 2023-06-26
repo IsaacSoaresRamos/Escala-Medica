d7464b593a2f1fb6e7480b86f0ff48d0e1d96c2c
Luy Mattos <112789465+LuyMattosS@users.noreply.github.com>
2023-06-26 04:32:05 -0300

    /

--- del_esp.php
+++ del_esp.php
@@ -46,12 +46,35 @@ else {
 ?>
 
 <!DOCTYPE html>
-<html lang="pt-br" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Pagina de delte especialidade</title>
-  </head>
-  <body>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Especialidade</title>
+    <!-- Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <!-- CSS -->
+        <link rel="stylesheet" href="css/del_esp.css">
+</head>
+  <body class="wallpaper"> 
+    <header class="hdr"> <!--Cabeçalho-->
+        <figure class="logo">
+            <img src="img/logo.png">
+        </figure>
+               
+        <nav class="menu"> <!--Menu-->
+            <div class="packge-menu">
+                <section class="pack-menu">
+                    <a href="tabela_adm.php"><p type="button" >Escala</p></a>
+                    <a href="index_logado_adm.php"><p type="button">Gerenciamento</p></a>
+                    <a href="sobre.php"><p type="button" >Sobre</p></a>
+                    <a href="Logout.php" class="link-login"><button type="login">Sair</button></a>
+                </section>
+            </div>
+        </nav>
+    </header>
     <h1>Apagar Especialidade</h1>
 
     <form action="processa_esp.php" method="post">
