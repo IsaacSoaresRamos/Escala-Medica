d224222c011a1ff401d4b0cd5e7aad60296c1af6
Luy Mattos <112789465+LuyMattosS@users.noreply.github.com>
2023-06-25 22:50:37 -0300

    /

--- index_logado_serv.php
+++ index_logado_serv.php
@@ -19,6 +19,10 @@ session_start();//Inicia a seção
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title>Minha Área</title>
+     <!-- Font Google -->
+          <link rel="preconnect" href="https://fonts.googleapis.com">
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+          <link href="https://fonts.googleapis.com/css2?family=Lexend+Peta:wght@500&family=Noto+Sans&display=swap" rel="stylesheet">
      <!-- Bootstrap -->
           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
@@ -40,6 +44,11 @@ session_start();//Inicia a seção
                          </section>
                     </div>
                </nav>
+
+               <div class="barra-conect">
+                    <h1>Olá <strong><?php echo $_SESSION['nome'];?></strong>!</h1>
+                    <h2>Conectado a Escala.Net</h2>
+               </div>
           </header>
           <?php if (!empty($_GET['msgErro'])) 
           { 
@@ -53,8 +62,8 @@ session_start();//Inicia a seção
           ?>
     <main class="packege"> <!--Conteudo-->
       <header class="pack-titulo">
-        <h1>Olá <strong><?php echo $_SESSION['nome'];?></strong>!</h1>
-        <h2>Sua agenda de gerenciamento<br/>de profissionais da saúde.</h2>
+        <h3>Escala.Net</h3>
+        <h4>Sua agenda de gerenciamento de profissionais da saúde.</h4>
       </header>
     </main> 
   </body>
